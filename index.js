c84d8b7db02c7ea7998e130dd09a077be8fadb17
Duane Grell <duanegrell@gmail.com>
2022-12-31 13:38:05 -0500

    commit tree 4096af6059aef61c777a4eb6954aea90ca02059e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const message = []
+
+function writeCards (names, event) {
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+writeCards (message);
+
+
+function countDown (integer) {
+    let count = integer;
+    while (count > -1) {
+        console.log(count--);
+    }
+}
